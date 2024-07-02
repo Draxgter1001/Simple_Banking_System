8180392f70b6ae80b5912e6b063141ef48bb2e9a
Tafshi Uthshow Hoque <uthshow@gmail.com>
2024-07-02 17:26:47 +0100

    Store information of a transaction

---
+++ src/Transaction.java
@@ -0,0 +1,18 @@
+public class Transaction {
+
+    private final int accountNumber;
+    private final String name;
+    private final double amount;
+
+    Transaction(int accountNumber, String name, double amount) {
+        this.accountNumber = accountNumber;
+        this.name = name;
+        this.amount = amount;
+
+    }
+
+    public String toString() {
+        return accountNumber + " " + name + " " + amount;
+    }
+
+}
