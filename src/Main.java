80a27a40c1d37c2d37883ec0d21be817ff8bdaf2
Tafshi Uthshow Hoque <uthshow@gmail.com>
2024-07-05 15:23:24 +0100

    Simplified main method

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
-import Custom_Exceptions.InvalidValue;
-
 public class Main {
 
 
     public static void main(String[] args){
-        App bank = new App();
-        bank.app();
+        App.app();
     }
 }
